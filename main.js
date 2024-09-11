6275f4d3152384bb3e7133aa762e6ec49dcd5a7c
ameera2443 <ameeramaherhussain@gmail.com>
2024-09-11 16:59:31 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function addClass(){
+document.querySelector("div").classList.add("plus");
+document.querySelector("div").classList.remove("minse");
+}
+
+
+
+function addClass1(){
+document.querySelector("div").classList.add("minse");
+document.querySelector("div").classList.remove("plus");
+}
+
+document.querySelector(".pluBtn").onclick=addClass;
+document.querySelector(".minBtn").onclick=addClass1;
+
+
